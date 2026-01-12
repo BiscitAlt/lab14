571ebea4de17c83f90e85729fd1e1428d704d171
BiscitAlt <thitiputt.pe@gmail.com>
2026-01-13 01:14:14 +0700

    lab14_1 completed

--- lab14_1.cpp
+++ lab14_1.cpp
@@ -16,3 +16,15 @@ void insertionSort(T d[],int N){
 		cout << endl;
 	}
 }
+
+int main(){
+	int a[10] = {12,25,30,44,2,0,4,7,55,25};
+	cout << "Input Array:";
+	for(int i = 0; i < 10; i++) cout << a[i] << " ";
+	cout << "\n\n";
+	
+	insertionSort(a,10);
+	
+	cout << "\nSorted Array:";
+	for(int i = 0; i < 10; i++) cout << a[i] << " ";	
+}
--- lab14_2.cpp
+++ lab14_2.cpp
@@ -21,3 +21,50 @@ int main(){
 }
 
 //Do not modify source code above this line 
+
+void inputMatrix(double matrix[][N]){
+	string input;
+	for(int i = 0; i < N; i++){
+		cout << "Row " << i+1 << ": ";
+		getline(cin,input);
+		int col = 0;
+		string num = "";
+		for(int j = 0; j < (int)input.size(); j++){
+			if(input[j] == ' '){
+				if(num != ""){
+					matrix[i][col] = stod(num);
+					col++;
+					num = "";
+				}
+			}
+			else{
+				num += input[j];
+			}
+		}
+		if(num != ""){
+            matrix[i][col] = stod(num);
+        }
+	}
+}
+
+void findLocalMax(const double input[][N], bool output[][N]){
+	for(int i = 0; i < N; i++){
+		for(int j = 0; j < N; j++){
+			output[i][j] = false;
+		}
+	}
+	for(int i = 1; i < N; i++){
+		for(int j = 1; j < N; j++){
+			if(input[i][j] > input[i-1][j] && input[i][j] > input[i+1][j] && input[i][j] > input[i][j-1] && input[i][j] > input[i][j+1]){
+				output[i][j] = true;
+			}
+			else{
+				output[i][j] = false;
+			}
+		}
+	}
+}
+
+void showMatrix(const bool x[][N]){
+
+}
